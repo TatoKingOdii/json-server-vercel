8a5d30bc11211b96c03e45c857fe625dbfb93464
cdivers <cullen.divers@briebug.com>
2024-02-10 18:09:55 -0800

    Reenable writes

--- api/server.js
+++ api/server.js
@@ -4,32 +4,12 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-const router = jsonServer.router({
-    "tv-shows": [
-        {'id': '0', 'name': 'Family Guy', 'genre': 'Comedy', 'rating':  'PG-13', 'seasons': 13, 'score': '99'}
-    ],
-    "drinks": [
-        {id: '1', name: 'Water', proof: 0, ingredients: 'Water', instructions: 'Fill glass from tap'}
-    ],
-    "inventory": [
-        {"id":"fc3bfb93-400f-407d-926c-3e4f321659d7","name":"Baked Beans","quantity":100,"category":"Food","receiveDate":"2024-02-08T08:00:00.000Z","hasExpiration":true,"expirationDate":"2025-01-22T08:00:00.000Z"}
-    ],
-    "users": [
-        {user: 'bob', pass: 'ross'}
-    ],
-    "courses": [
-        {id: '1', title: 'Potato 1', description: 'A thing 1', percentageComplete: 26, favorite: true},
-        {id: '2', title: 'Potato 2', description: 'A thing 2', percentageComplete: 0, favorite: true}
-    ]
-  })
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 const middlewares = jsonServer.defaults()
 
